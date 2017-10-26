7cbc211650e97a02514007c42f5c8fd44ce25773
蒋萍萍 <pingping.jiang@3g2win.com>
2017-10-26 16:38:22 +0800

    修复新SDK与高德地图冲突问题

--- src/org/zywx/wbpalmstar/plugin/uexgaodenavi/MyAMapNaviListener.java
+++ src/org/zywx/wbpalmstar/plugin/uexgaodenavi/MyAMapNaviListener.java
@@ -21,15 +21,15 @@ package org.zywx.wbpalmstar.plugin.uexgaodenavi;
 
 import com.amap.api.navi.AMapNaviListener;
 import com.amap.api.navi.model.AMapLaneInfo;
+import com.amap.api.navi.model.AMapNaviCameraInfo;
 import com.amap.api.navi.model.AMapNaviCross;
 import com.amap.api.navi.model.AMapNaviInfo;
 import com.amap.api.navi.model.AMapNaviLocation;
-import com.amap.api.navi.model.AMapNaviStaticInfo;
 import com.amap.api.navi.model.AMapNaviTrafficFacilityInfo;
+import com.amap.api.navi.model.AMapServiceAreaInfo;
 import com.amap.api.navi.model.AimLessModeCongestionInfo;
 import com.amap.api.navi.model.AimLessModeStat;
 import com.amap.api.navi.model.NaviInfo;
-import com.autonavi.tbt.NaviStaticInfo;
 import com.autonavi.tbt.TrafficFacilityInfo;
 
 import org.zywx.wbpalmstar.base.BDebug;
@@ -111,17 +111,7 @@ public class MyAMapNaviListener implements AMapNaviListener {
     }
 
     @Override
-    public void onArriveDestination(NaviStaticInfo naviStaticInfo) {
-
-    }
-
-    @Override
-    public void onArriveDestination(AMapNaviStaticInfo aMapNaviStaticInfo) {
-
-    }
-
-    @Override
-    public void onCalculateRouteSuccess() {
+    public void onCalculateRouteSuccess(int[] var1) {
         mEUExGaodeNavi.callbackCalculateRoute(true);
 
     }
@@ -193,11 +183,6 @@ public class MyAMapNaviListener implements AMapNaviListener {
     }
 
     @Override
-    public void onCalculateMultipleRoutesSuccess(int[] ints) {
-
-    }
-
-    @Override
     public void notifyParallelRoad(int i) {
 
     }
@@ -214,6 +199,26 @@ public class MyAMapNaviListener implements AMapNaviListener {
 
     @Override
     public void updateAimlessModeCongestionInfo(AimLessModeCongestionInfo aimLessModeCongestionInfo) {
+
+    }
+
+    @Override
+    public void onServiceAreaUpdate(AMapServiceAreaInfo[] aMapServiceAreaInfos) {
+
+    }
+
+    @Override
+    public void updateCameraInfo(AMapNaviCameraInfo[] aMapNaviCameraInfos) {
+
+    }
+
+    @Override
+    public void onGetNavigationText(String s) {
+
+    }
+
+    @Override
+    public void onPlayRing(int i) {
 
     }
 }
