4f90e2794f6b1162293e4f5a8bb68b06d63b13aa
蒋萍萍 <pingping.jiang@3g2win.com>
2017-10-26 17:47:54 +0800

    修复新SDK与高德地图冲突问题

--- src/org/zywx/wbpalmstar/plugin/uexgaodenavi/MyAMapNaviListener.java
+++ src/org/zywx/wbpalmstar/plugin/uexgaodenavi/MyAMapNaviListener.java
@@ -21,10 +21,12 @@ package org.zywx.wbpalmstar.plugin.uexgaodenavi;
 
 import com.amap.api.navi.AMapNaviListener;
 import com.amap.api.navi.model.AMapLaneInfo;
+import com.amap.api.navi.model.AMapNaviCameraInfo;
 import com.amap.api.navi.model.AMapNaviCross;
 import com.amap.api.navi.model.AMapNaviInfo;
 import com.amap.api.navi.model.AMapNaviLocation;
 import com.amap.api.navi.model.AMapNaviTrafficFacilityInfo;
+import com.amap.api.navi.model.AMapServiceAreaInfo;
 import com.amap.api.navi.model.AimLessModeCongestionInfo;
 import com.amap.api.navi.model.AimLessModeStat;
 import com.amap.api.navi.model.NaviInfo;
@@ -100,7 +102,7 @@ public class MyAMapNaviListener implements AMapNaviListener {
     }
 
     @Override
-    public void onCalculateRouteSuccess() {
+    public void onCalculateRouteSuccess(int[] var1) {
         mEUExGaodeNavi.callbackCalculateRoute(true);
 
     }
@@ -172,11 +174,6 @@ public class MyAMapNaviListener implements AMapNaviListener {
     }
 
     @Override
-    public void onCalculateMultipleRoutesSuccess(int[] ints) {
-
-    }
-
-    @Override
     public void notifyParallelRoad(int i) {
 
     }
@@ -193,6 +190,26 @@ public class MyAMapNaviListener implements AMapNaviListener {
 
     @Override
     public void updateAimlessModeCongestionInfo(AimLessModeCongestionInfo aimLessModeCongestionInfo) {
+
+    }
+
+    @Override
+    public void onServiceAreaUpdate(AMapServiceAreaInfo[] aMapServiceAreaInfos) {
+
+    }
+
+    @Override
+    public void updateCameraInfo(AMapNaviCameraInfo[] aMapNaviCameraInfos) {
+
+    }
+
+    @Override
+    public void onGetNavigationText(String s) {
+
+    }
+
+    @Override
+    public void onPlayRing(int i) {
 
     }
 }
